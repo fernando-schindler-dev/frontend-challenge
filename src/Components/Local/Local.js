16a040f4fd44eb39ed563d12bd512df4e210e9af
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-06-03 23:24:08 -0300

    Adicionado o conteúdo e o estilo da seção local

--- src/Components/Local/Local.js
+++ src/Components/Local/Local.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import styles from './Local.module.css';
 
+import { ReactComponent as Map } from '../../Assets/map.svg';
+
+import InformationWithImage from '../../Assistents/InformationWithImage/InformationWithImage';
+
 const Local = () => (
   <section className={`${styles.container} item`}>
     <h2>Local</h2>
+
+    <div className={styles.image}></div>
+
+    <InformationWithImage
+      image={<Map />}
+      main="Avenida Brasil, 4019"
+      complement="Trabalho"
+    />
   </section>
 );
 
