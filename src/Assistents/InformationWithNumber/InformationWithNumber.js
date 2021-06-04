f23bb6c49004d5b7f728dddb5dceab53c201113e
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-06-04 00:20:16 -0300

    Adicionado o conteúdo e estilo da seção oportunidades; adicionado um svg

--- src/Assistents/InformationWithNumber/InformationWithNumber.js
+++ src/Assistents/InformationWithNumber/InformationWithNumber.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import styles from './InformationWithNumber.module.css';
 
-const InformationWithNumber = () => <div className={styles.container}></div>;
+const InformationWithNumber = (props) => (
+  <div className={styles.container}>
+    <div className={styles.number} style={{ background: props.color }}>
+      {props.number}
+    </div>
+
+    <div className={styles.informations}>
+      <p>{props.main}</p>
+      <p>{props.complement}</p>
+    </div>
+  </div>
+);
 
 export default InformationWithNumber;
