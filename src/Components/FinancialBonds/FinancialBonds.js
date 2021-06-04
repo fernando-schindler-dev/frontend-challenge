932433e11825054edaef8901f9eeb7c391e93aea
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-06-04 01:09:43 -0300

    Adicionado o conteúdo e o estilo da seção títulos financeiros; alterado o align-content do conteúdo principal para start

--- src/Components/FinancialBonds/FinancialBonds.js
+++ src/Components/FinancialBonds/FinancialBonds.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import styles from './FinancialBonds.module.css';
 
+import InformationWithNumber from '../../Assistents/InformationWithNumber/InformationWithNumber';
+
 const FinancialBonds = () => (
   <section className={`${styles.container} item`}>
-    <h2>Títulos financeiros</h2>
+    <h2 className={styles.title}>Títulos financeiros</h2>
+
+    <InformationWithNumber
+      color="#C62828"
+      number="1"
+      main="R$ 3.105,00"
+      complement="Vencidos"
+    />
+
+    <InformationWithNumber
+      color="#FF6F00"
+      number="2"
+      main="R$ 3.105,00"
+      complement="A vencer"
+    />
+
+    <InformationWithNumber
+      color="#2E7D32"
+      number="2"
+      main="R$ 3.105,00"
+      complement="Pagos"
+    />
   </section>
 );
 
