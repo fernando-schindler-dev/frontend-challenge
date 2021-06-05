46e9aca4ee98e9b21159c4be05e6620b080037cf
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-06-05 01:10:44 -0300

    Adicionado o conteúdo e o estilo da seção local; adicionado o componente auxiliar maps

--- src/Components/Local/Local.js
+++ src/Components/Local/Local.js
@@ -4,12 +4,17 @@ import styles from './Local.module.css';
 import { ReactComponent as Map } from '../../Assets/map.svg';
 
 import InformationWithImage from '../../Assistents/InformationWithImage/InformationWithImage';
+import Loading from '../../Assistents/Loading/Loading';
+
+const Maps = React.lazy(() => import('../../Assistents/Maps/Maps'));
 
 const Local = () => (
   <section className={`${styles.container} item`}>
     <h2>Local</h2>
 
-    <div className={styles.image}></div>
+    <React.Suspense fallback={<Loading />}>
+      <Maps />
+    </React.Suspense>
 
     <InformationWithImage
       image={<Map />}
