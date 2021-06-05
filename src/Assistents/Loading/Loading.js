2ecaacdb704a8eef0c89c3ec388c723a8a57da06
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-06-05 00:26:18 -0300

    Adicionado a biblioteca victory ao projeto; adicionado o conteúdo e o estilo da seção vendas; adicionado dois componentes auxiliares, sendo eles o graph e o loading

---
+++ src/Assistents/Loading/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Loading.module.css';
+
+const Loading = () => (
+  <div className={styles.container}>
+    <span className={styles.circle}></span>
+  </div>
+);
+
+export default Loading;
--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Loading from '../../Assistents/Loading/Loading';
 import styles from './Sales.module.css';
+
+const Graph = React.lazy(() => import('../../Assistents/Graph/Graph'));
 
 const Sales = () => (
   <section className={`${styles.container} item`}>
     <h2>Vendas</h2>
+
+    <React.Suspense fallback={<Loading />}>
+      <Graph />
+    </React.Suspense>
   </section>
 );
 
